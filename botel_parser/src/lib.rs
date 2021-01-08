88c226dab742191b0c9bec74b27e466e3dbca855
uigctaw <uigctaw@protonmail.com>
2021-01-08 01:30:24 +0000

    some initial stuff in tokenizer

--- botel_parser/src/lib.rs
+++ botel_parser/src/lib.rs
@@ -7,7 +7,7 @@ pub fn parse(tokens: &Vec<Token>) -> () {
 
 
 #[cfg(test)]
-mod tests {
+mod tests 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
--- botel_tokenizer/src/lib.rs
+++ botel_tokenizer/src/lib.rs
@@ -1,15 +1,61 @@
-pub struct Token {}
+use regex::Regex;
+use lazy_static::lazy_static;
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum Token {
+    Whitespace(String),    
+    Equals(String),    
+}
+
+
+lazy_static! {
+    static ref REGEX_AND_KIND: Vec<(Regex, fn(String) -> Token)> = vec![
+        (Regex::new(r"^\s+").unwrap(), Token::Whitespace),
+        (Regex::new(r"^=").unwrap(), Token::Equals),
+        //(Regex::new(r"^\d+[\s$]").unwrap(), TokenKind::Int),
+        //(Regex::new(r"^[_A-Za-z][_A-Za-z0-9]*").unwrap(), TokenKind::Name),
+    ];
+}
+
 
 
 pub fn tokenize(program_text: &str) -> Vec<Token> {
-    vec![]
+    let mut tokens = Vec::new();
+    let mut slice = program_text;
+    while slice.len() > 0 {
+        for (regex, token_kind) in REGEX_AND_KIND.iter() {
+            if let Some(match_) = regex.find(slice) {
+                let matched_slice = slice[.. match_.end()].to_string();
+                tokens.push(token_kind(matched_slice));
+                slice = &slice[match_.end() ..];
+                break;
+            }
+        }
+    }
+    tokens
 }
 
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn find_whitespace() {
+        let tokens = tokenize("  ");
+        assert_eq!(tokens, vec![Token::Whitespace("  ".to_string())]);
+    }
+
+    #[test]
+    fn find_equals() {
+        let tokens = tokenize("==");
+        assert_eq!(
+            tokens, 
+            vec![
+                Token::Equals("=".to_string()),
+                Token::Equals("=".to_string()),
+            ],
+        );
     }
 }
