6f7cd326a672c7954d6f799dedfa1587e8b32f40
uigctaw <uigctaw@protonmail.com>
2021-01-17 23:42:01 +0000

    some basic tokenization and parsing

--- botel_parser/src/lib.rs
+++ botel_parser/src/lib.rs
@@ -1,15 +1,192 @@
 use botel_tokenizer::Token;
+//use std::collections::VecDeque;
+use std::boxed::Box;
 
 
-pub fn parse(tokens: &Vec<Token>) -> () {
-    
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum AST {
+    BinOp(BinOp),
+    Name(String),
+    Integer(i64),  // This will need a designeted type
+}
+
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub enum BinOpName {
+    Equation,
+    Sum,
+}
+
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+pub struct BinOp{
+    left: Box<AST>,
+    right: Box<AST>,
+    what: BinOpName,
+}
+
+
+pub fn parse(tokens: Vec<Token>) -> Vec<AST> {
+    let postfix_tokens = convert_to_postfix_notation(tokens);
+    let mut ast = Vec::new();
+    for token in postfix_tokens.into_iter() {
+        match token {
+            Token::Name(name) => {
+                ast.push(AST::Name(name))
+            },
+            Token::Integer(num) => { 
+                ast.push(AST::Integer(num.parse::<i64>().unwrap()));
+            },
+            Token::Equals(_) => {
+                let maybe_right = ast.pop();
+                let maybe_left = ast.pop();
+                if let (Some(left), Some(right)) = (maybe_left, maybe_right) {
+                    let eq = BinOp {
+                        left: Box::new(left),
+                        right: Box::new(right),
+                        what: BinOpName::Equation,
+                    };
+                    ast.push(AST::BinOp(eq));
+                } else { panic!("some syntax error") }
+            },
+            Token::Plus(_) => {
+                let maybe_right = ast.pop();
+                let maybe_left = ast.pop();
+                if let (Some(left), Some(right)) = (maybe_left, maybe_right) {
+                    let sum = BinOp {
+                        left: Box::new(left),
+                        right: Box::new(right),
+                        what: BinOpName::Sum,
+                    };
+                    ast.push(AST::BinOp(sum));
+                } else { panic!("some syntax error") }
+            },
+            _ => panic!("unexpected token"),
+        }
+    }
+
+    ast
+}
+
+
+fn convert_to_postfix_notation(tokens: Vec<Token>) -> Vec<Token> {
+    let mut postfix_tokens = Vec::new();
+    let mut operator_stack = Vec::new();
+
+    for token in tokens.into_iter() {
+        match token {
+            Token::Equals(_) => { operator_stack.push(token) },
+            Token::Plus(_) => { operator_stack.push(token) },
+            _ => { postfix_tokens.push(token) },
+        }
+    }
+    for operator in operator_stack.into_iter().rev() {
+        postfix_tokens.push(operator);
+    }
+
+    postfix_tokens
 }
 
 
 #[cfg(test)]
-mod tests 
+mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn just_name_or_integer() {
+        let result = parse(vec![Token::Name("Hi".to_string())]);
+        assert_eq!(result, vec![AST::Name("Hi".to_string())]);
+
+        let result = parse(vec![Token::Integer("1234".to_string())]);
+        assert_eq!(result, vec![AST::Integer(1234)]);
+    }
+
+    #[test]
+    fn equation_name_equals_value_to_postfix_notation() {
+        let tokens = vec![
+            Token::Name("Hi".to_string()),
+            Token::Equals("does not matter".to_string()),
+            Token::Integer("1234".to_string()),
+        ];
+        let postfixed = convert_to_postfix_notation(tokens);
+        assert_eq!(
+            postfixed,
+            vec![
+                Token::Name("Hi".to_string()),
+                Token::Integer("1234".to_string()),
+                Token::Equals("does not matter".to_string()),
+            ],
+        );
+    }
+
+    #[test]
+    fn equation_name_equals_value() {
+        let tokens = vec![
+            Token::Name("Hi".to_string()),
+            Token::Equals("does not matter".to_string()),
+            Token::Integer("1234".to_string()),
+        ];
+        let ast = parse(tokens);
+        assert_eq!(
+            ast,
+            vec![
+                AST::BinOp(BinOp {
+                    left: Box::new(AST::Name("Hi".to_string())),
+                    right: Box::new(AST::Integer(1234)),
+                    what: BinOpName::Equation,
+                }),
+            ],
+        );
+    }
+
+    #[test]
+    fn equation_name_equals_sum_to_postfix_notation() {
+        let tokens = vec![
+            Token::Name("Hi".to_string()),
+            Token::Equals("does not matter".to_string()),
+            Token::Integer("2".to_string()),
+            Token::Plus("also does not matter".to_string()),
+            Token::Name("Hello".to_string()),
+        ];
+        let postfixed = convert_to_postfix_notation(tokens);
+        assert_eq!(
+            postfixed,
+            vec![
+            Token::Name("Hi".to_string()),
+            Token::Integer("2".to_string()),
+            Token::Name("Hello".to_string()),
+            Token::Plus("also does not matter".to_string()),
+            Token::Equals("does not matter".to_string()),
+            ],
+        );
+    }
+
+    #[test]
+    fn equation_name_equals_sum() {
+        let tokens = vec![
+            Token::Name("Hi".to_string()),
+            Token::Equals("does not matter".to_string()),
+            Token::Integer("2".to_string()),
+            Token::Plus("also does not matter".to_string()),
+            Token::Name("Hello".to_string()),
+        ];
+        let ast = parse(tokens);
+        assert_eq!(
+            ast,
+            vec![
+                AST::BinOp(BinOp {
+                    left: Box::new(AST::Name("Hi".to_string())),
+                    right: Box::new(AST::BinOp(BinOp {
+                        left: Box::new(AST::Integer(2)),
+                        right: Box::new(AST::Name("Hello".to_string())),
+                        what: BinOpName::Sum,
+                    })),
+                    what: BinOpName::Equation,
+                }),
+            ],
+        );
     }
 }
--- botel_tokenizer/src/lib.rs
+++ botel_tokenizer/src/lib.rs
@@ -1,11 +1,15 @@
 use regex::Regex;
 use lazy_static::lazy_static;
 
+
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum Token {
     Whitespace(String),    
     Equals(String),    
+    Integer(String),
+    Name(String),
+    Plus(String),
 }
 
 
@@ -13,25 +17,33 @@ lazy_static! {
     static ref REGEX_AND_KIND: Vec<(Regex, fn(String) -> Token)> = vec![
         (Regex::new(r"^\s+").unwrap(), Token::Whitespace),
         (Regex::new(r"^=").unwrap(), Token::Equals),
-        //(Regex::new(r"^\d+[\s$]").unwrap(), TokenKind::Int),
-        //(Regex::new(r"^[_A-Za-z][_A-Za-z0-9]*").unwrap(), TokenKind::Name),
+        (Regex::new(r"^(\d+)(?:\s|$)").unwrap(), Token::Integer),
+        (Regex::new(r"^[_A-Za-z][_A-Za-z0-9]*").unwrap(), Token::Name),
     ];
 }
-
 
 
 pub fn tokenize(program_text: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
     let mut slice = program_text;
     while slice.len() > 0 {
+        let mut matched = false;
         for (regex, token_kind) in REGEX_AND_KIND.iter() {
-            if let Some(match_) = regex.find(slice) {
+            if let Some(maybe_captures) = regex.captures(slice) {
+                let match_ = match maybe_captures.len() {
+                    1 => maybe_captures.get(0).unwrap(),
+                    2 => maybe_captures.get(1).unwrap(),
+                    _ => panic!("Too many captures!"),
+                };
+                if match_.start() != 0 { panic!("Wat?!") };
                 let matched_slice = slice[.. match_.end()].to_string();
                 tokens.push(token_kind(matched_slice));
                 slice = &slice[match_.end() ..];
+                matched = true;
                 break;
             }
         }
+        if !matched { panic!("Could not parse {}", slice) };
     }
     tokens
 }
@@ -55,6 +67,36 @@ mod tests {
             vec![
                 Token::Equals("=".to_string()),
                 Token::Equals("=".to_string()),
+            ],
+        );
+    }
+
+    #[test]
+    fn find_integer() {
+        let tokens = tokenize("12 34");
+        assert_eq!(
+            tokens, 
+            vec![
+                Token::Integer("12".to_string()),
+                Token::Whitespace(" ".to_string()),
+                Token::Integer("34".to_string()),
+            ],
+        );
+    }
+
+    #[test]
+    fn find_name() {
+        let tokens = tokenize("a _ Bc _d");
+        assert_eq!(
+            tokens, 
+            vec![
+                Token::Name("a".to_string()),
+                Token::Whitespace(" ".to_string()),
+                Token::Name("_".to_string()),
+                Token::Whitespace(" ".to_string()),
+                Token::Name("Bc".to_string()),
+                Token::Whitespace(" ".to_string()),
+                Token::Name("_d".to_string()),
             ],
         );
     }
